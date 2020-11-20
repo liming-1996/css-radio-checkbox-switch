1866057a235f3cd741a40bfd9754e2dea7e22319
lmdpx <1628883533@qq.com>
2020-11-20 15:54:33 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                files: {
+                    'dist/css/radio-checkbox-switch.css': [
+                        'build/less/common.less',
+                        'build/less/radio.less',
+                        'build/less/checkbox.less',
+                        'build/less/switch.less',
+                    ]
+                }
+            }
+        },
+        cssmin: {
+            build: {
+                src: 'dist/css/radio-checkbox-switch.css',
+                dest: 'dist/css/radio-checkbox-switch.min.css'
+            }
+        },
+        watch: {
+            less: {
+                files: ['build/less/*.less'],
+                tasks: ['default'],
+            },
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('watcher', ['watch']);
+    grunt.registerTask('default', ['less', 'less', 'cssmin']);
+};
